65e3f014876618af2c9c227e98b19ccb8377733a
Aykadia <144953576+Aykadia@users.noreply.github.com>
2024-03-01 23:55:04 +0100

    Add files via upload

---
+++ Ayk-Command/Ayk-Command/config.lua
@@ -0,0 +1,15 @@
+Config                  = {}
+
+-- This is the config of Ayk-Command
+
+Config.NameServer = "SERVERNAME" -- put your server name
+
+Config.DiscordLink = "discord.gg/**" -- put your discord server link
+
+Config.Notif = "okok" -- choose your notif system become -- "QB" ; "okok" ; "vms" ; "forge" ; "brutal" or "none" if you don't have any notif system
+
+Config.F8 = true -- if you want the discord links to appear in F8 put "true" and "false" if you don't want 
+
+Config.Language = "en" -- choose your language only fr or en for the moment 
+
+-- Enjoy !! 
